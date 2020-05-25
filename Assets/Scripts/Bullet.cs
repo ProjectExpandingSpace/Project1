c9a4da0c4aab3b4880969dbcfc56a7539576f720
Daan Kikkert <54845837+DaanKikkert@users.noreply.github.com>
2020-05-25 09:15:30 +0200

    sprint 1 resultaat

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Bullet : MonoBehaviour
+{
+    private Vector2 moveDirection;
+    public int bulletLifetime = 2;
+    public int bulletSpeed = 10;
+    public int bulletDamage = 20;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Invoke("Destroy", bulletLifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * bulletSpeed * Time.deltaTime);
+    }
+
+    public void SetMoveDirection(Vector2 dir)
+    {
+        moveDirection = dir;
+    }
+
+    private void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerHealth>().takeDamage(bulletDamage);
+            Invoke("Destroy", 0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            takeDamage(20);
+        }
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void takeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        healthBar.setHealth(currentHealth);
+    }
+
+    private void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
