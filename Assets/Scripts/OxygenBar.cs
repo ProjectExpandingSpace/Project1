2f001ee95874d65da620fac5a19bf31e04daa9cc
ProDutchchees <reynando101@hotmail.com>
2020-05-28 10:46:14 +0200

    Create OxygenBar.cs

---
+++ Assets/Scripts/OxygenBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenBar : MonoBehaviour
+
+{
+
+    public float Oxygen = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Oxygen = Oxygen - Time.deltaTime;
+        Debug.Log(Oxygen);
+    }
+}
